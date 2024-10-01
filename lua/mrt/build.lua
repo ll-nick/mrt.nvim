3cf471e5da99a31717c49036d0ef806e3a4512d2
Nick Le Large <nick.lelarge@kit.edu>
2024-10-01 10:19:36 +0200

    Set errorformat

--- lua/mrt/build.lua
+++ lua/mrt/build.lua
@@ -54,6 +54,40 @@ local function run_build(flags)
 
 	-- Set makeprg to the shell script, passing the directory and flags as arguments
 	vim.opt_local.makeprg = script .. " " .. vim.fn.shellescape(workspace_root) .. " " .. vim.fn.shellescape(flags)
+	-- Set custom errorformat to parse the output of the build script
+	vim.opt_local.errorformat = {
+		-- Push build directory to directory stack to match relative paths of later error messages
+		"%DErrors%\\s%\\+<< %\\w%\\+:%\\w%\\+ %f/build%.%#.log",
+		-- Start error multiline error message
+		"%E%f:%l:%c: %t%*[^:]: %m",
+		-- Continue error multiline error message while there is a |
+		"%C%.%#|%.%#",
+		-- Default errorformat entries
+		'%*[^"]"%f"%*\\D%l: %m',
+		'"%f"%*\\D%l: %m',
+		"%-Gg%\\?make[%*\\d]: *** [%f:%l:%m",
+		"%-Gg%\\?make: *** [%f:%l:%m",
+		"%-G%f:%l: (Each undeclared identifier is reported only once",
+		"%-G%f:%l: for each function it appears in.)",
+		"%-GIn file included from %f:%l:%c:",
+		"%-GIn file included from %f:%l:%c\\,",
+		"%-GIn file included from %f:%l:%c",
+		"%-GIn file included from %f:%l",
+		"%-G%*[ ]from %f:%l:%c",
+		"%-G%*[ ]from %f:%l:",
+		"%-G%*[ ]from %f:%l\\,",
+		"%-G%*[ ]from %f:%l",
+		"%f:%l:%c:%m",
+		"%f(%l):%m",
+		"%f:%l:%m",
+		'"%f"\\, line %l%*\\D%c%*[^ ] %m',
+		"%D%*\\a[%*\\d]: Entering directory %*[`']%f'",
+		"%X%*\\a[%*\\d]: Leaving directory %*[`']%f'",
+		"%D%*\\a: Entering directory %*[`']%f'",
+		"%X%*\\a: Leaving directory %*[`']%f'",
+		"%DMaking %*\\a in %f",
+		"%f|%l| %m",
+	}
 
 	if has_dispatch() then
 		-- Use :Make (from tpope/dispatch) to run in the background
