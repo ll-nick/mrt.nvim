7a71494cbdf4ed758ba2d29a620715a51f3b6be2
Nick Le Large <nick.lelarge@kit.edu>
2025-03-01 22:47:49 +0100

    Refactor init.lua by running workspace check in extra function

--- lua/mrt/init.lua
+++ lua/mrt/init.lua
@@ -1,5 +1,3 @@
-local mrt = {}
-
 local Path = require("plenary.path")
 
 local catkin_profile = require("mrt.catkin_profile")
@@ -8,58 +6,40 @@ local mrt_overseer = require("mrt.overseer")
 local package_picker = require("mrt.package_picker")
 local utils = require("mrt.utils")
 
+local run_command_if_in_workspace = function(command)
+    local cwd = Path:new(vim.fn.getcwd())
+    if not utils.is_in_catkin_workspace(cwd) then
+        vim.notify("Command must be called from inside a catkin workspace.")
+        return
+    end
+
+    command()
+end
+
+local mrt = {}
+
 mrt.setup = config.setup
 
 mrt_overseer.register_templates()
 
 mrt.build_workspace = function()
-    local cwd = Path:new(vim.fn.getcwd())
-    if not utils.is_in_catkin_workspace(cwd) then
-        vim.notify("Command must be called from inside a catkin workspace.")
-        return
-    end
-
-    mrt_overseer.build_workspace()
+    run_command_if_in_workspace(mrt_overseer.build_workspace)
 end
 
 mrt.build_current_package = function()
-    local cwd = Path:new(vim.fn.getcwd())
-    if not utils.is_in_catkin_workspace(cwd) then
-        vim.notify("Command must be called from inside a catkin workspace.")
-        return
-    end
-
-    mrt_overseer.build_current_package()
+    run_command_if_in_workspace(mrt_overseer.build_current_package)
 end
 
 mrt.build_current_package_tests = function()
-    local cwd = Path:new(vim.fn.getcwd())
-    if not utils.is_in_catkin_workspace(cwd) then
-        vim.notify("Command must be called from inside a catkin workspace.")
-        return
-    end
-
-    mrt_overseer.build_current_package_tests()
+    run_command_if_in_workspace(mrt_overseer.build_current_package_tests)
 end
 
 mrt.switch_catkin_profile = function()
-    local cwd = Path:new(vim.fn.getcwd())
-    if not utils.is_in_catkin_workspace(cwd) then
-        vim.notify("Command must be called from inside a catkin workspace.")
-        return
-    end
-
-    catkin_profile.switch_profile_ui()
+    run_command_if_in_workspace(catkin_profile.switch_profile_ui)
 end
 
 mrt.pick_catkin_package = function()
-    local cwd = Path:new(vim.fn.getcwd())
-    if not utils.is_in_catkin_workspace(cwd) then
-        vim.notify("Command must be called from inside a catkin workspace.")
-        return
-    end
-
-    package_picker.pick_catkin_package()
+    run_command_if_in_workspace(package_picker.pick_catkin_package)
 end
 
 return mrt
